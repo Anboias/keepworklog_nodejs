ca7646dd3d29bc555c5966408fd6622d27e915b8
Anboias <biasinovschi@gmail.com>
2020-12-04 23:49:07 +0200

    moved firebase in a separate file

---
+++ data/index.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    status: 'open',
+    date: '20201204',
+    content: 'Fill out human interest distribution form',
+  },
+  {
+    id: 2,
+    status: 'open',
+    date: '20201204',
+    content: 'Get an anniversary gift',
+  },
+  {
+    id: 3,
+    status: 'done',
+    date: '20201204',
+    content: 'Call the bank to talk about investments',
+  },
+  {
+    id: 4,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 5,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 6,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 7,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+];
+
+export { data };
