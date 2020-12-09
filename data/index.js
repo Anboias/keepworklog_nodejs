2977e5890c73aab39e0214e654237bdb58ce73ea
Bogdan Iasinovschi <biasinovschi@gmail.com>
2020-12-10 00:33:53 +0200

    Create index.js

---
+++ data/index.js
@@ -0,0 +1,118 @@
+const data = [
+  {
+    id: 1,
+    status: 'open',
+    date: '20201204',
+    content: 'Fill out human interest distribution form',
+  },
+  {
+    id: 2,
+    status: 'open',
+    date: '20201204',
+    content: 'Get an anniversary gift',
+  },
+  {
+    id: 3,
+    status: 'open',
+    date: '20201204',
+    content: 'Call the bank to talk about investments',
+  },
+  {
+    id: 4,
+    status: 'open',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 5,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 6,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 7,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 8,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 9,
+    status: 'done',
+    date: '20201204',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 10,
+    status: 'done',
+    date: '20210104',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 11,
+    status: 'done',
+    date: '20210105',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 12,
+    status: 'done',
+    date: '20210107',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 13,
+    status: 'done',
+    date: '20210104',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 14,
+    status: 'done',
+    date: '20210112',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 15,
+    status: 'done',
+    date: '20210115',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 16,
+    status: 'done',
+    date: '20210104',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 17,
+    status: 'done',
+    date: '20210104',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 18,
+    status: 'done',
+    date: '20210104',
+    content: 'Finish reading Intro to UI/UX',
+  },
+  {
+    id: 19,
+    status: 'done',
+    date: '20210104',
+    content: 'Finish reading Intro to UI/UX',
+  },
+];
+
+export default data;
