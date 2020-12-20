0b4bb36caad7ed4104b584a312ba4cc816282d22
Anboias <biasinovschi@gmail.com>
2020-12-20 17:04:17 +0200

    set offset to getweeknumber to start week from Monday

--- utils/getDateRangeOfWeek.js
+++ utils/getDateRangeOfWeek.js
@@ -33,7 +33,8 @@ Date.prototype.getWeekPrototype = function (numOfdaysPastSinceLastMonday) {
 Date.prototype.getWeekNumberPrototype = function (dowOffset) {
   /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
 
-  dowOffset = typeof dowOffset == 'int' ? dowOffset : 0; //default dowOffset to zero
+  // dowOffset = typeof dowOffset == 'int' ? dowOffset : 0; //default dowOffset to zero
+  dowOffset = 1;
   var newYear = new Date(this.getFullYear(), 0, 1);
   var day = newYear.getDay() - dowOffset; //the day of week the year begins on
   day = day >= 0 ? day : day + 7;
